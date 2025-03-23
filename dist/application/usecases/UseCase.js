8cf9a19bf3b09c5171d3f3f76d1e5bb41200d916
Ahmet Asaad Hammoud <88238215+ahmed741321@users.noreply.github.com>
2025-03-23 08:30:23 +0100

    Delete dist directory

--- dist/application/usecases/UseCase.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UseCase = void 0;
-class UseCase {
-}
-exports.UseCase = UseCase;
--- dist/presentation/controllers/Controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controller = void 0;
-class Controller {
-    handleRequest(request) {
-        // Logic to handle incoming requests
-    }
-}
-exports.Controller = Controller;
